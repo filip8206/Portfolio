bbbef90cd96f035a1022872074cdc7060ae93f57
filip8206 <fhensels@gmail.com>
2024-02-09 22:14:51 +0100

    update for responsiveness

---
+++ js/header.js
@@ -0,0 +1,4 @@
+let headerButton = document.getElementById('headerButton')
+let headerElement = document.querySelector('header')
+
+headerButton.addEventListener('click', () => headerElement.classList.toggle('mobileExpanded'))
